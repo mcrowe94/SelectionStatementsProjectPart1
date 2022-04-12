e5a7b17706d80b8e30c4a4c6c903acc4a2b660a6
Monica Crowe <mcrowe9194@gmail.com>
2022-04-12 18:27:24 -0500

    Add project files.

---
+++ SelectionStatementsProjectPart1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SelectionStatementsProjectPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rng = new Random();
+            int randomNumber = rng.Next(1, 1000);
+            while (true)
+            {
+                Console.WriteLine("Welcome to your random number generator!");
+                Console.WriteLine("A random number has been selected! Can you guess the correct number?");
+                
+
+                string input = Console.ReadLine();
+                int userGuess = int.Parse(input);
+
+                if (userGuess == randomNumber)
+                {
+                    Console.WriteLine($"Correct! Press enter to exit!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.WriteLine($"Too high, try again!");
+                }
+                else if (userGuess < randomNumber)
+                {
+                    Console.WriteLine($"Too low, try again!");                    
+                }               
+            }
+
+        }
+    }
+}
+
